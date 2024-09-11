73796058a95d890ce56ca1f97ec385132c7f623b
Rensytims <rensytimsy03@gmail.com>
2024-09-12 01:06:33 +0300

    done with the landing page

---
+++ client/src/websitePages/footer.jsx
@@ -0,0 +1,31 @@
+import react from "react";
+import "../styleSheets/hotel.css"
+
+export default function Footer(){
+    return(
+        <>
+            <div className="footer">
+                <div className="colums">
+                    <h1>Reach to us via:</h1>
+                    <p>travelhotel@gmail.com</p>
+                    <p>Line: +254790337192</p>
+                    <p>Instagram: TravelHotelGlobal</p>
+                    <p>FaceBook: TravelHotelGlobal</p>
+                    <p>Twitter: travelhotelglobal</p>
+                </div>
+                <div className="colums">
+                    <h1>Located at</h1>
+                    <p>Nairobi, Kenya</p>
+                    <p>kiambu</p>
+                    <p>Office number 06</p>
+                </div>
+                <div className="colums">
+                    <h1>Developed By</h1>
+                    <p>Founder: Timothy Mutwir</p>
+                    <p>Co-founder: Joseph</p>
+                    <p>Copyright@travelhotelglobal:2024</p>
+                </div>
+            </div>
+        </>
+    )
+}
--- client/src/websitePages/homePage.jsx
+++ client/src/websitePages/homePage.jsx
@@ -6,29 +6,74 @@ import AboutPage from "./about.jsx";
 import { Button, Card, Typography } from "@mui/material";
 import { green } from "@mui/material/colors";
 import "../styleSheets/home-page.css"
+import ConnectingAirportsIcon from '@mui/icons-material/ConnectingAirports';
+import EmojiNatureIcon from '@mui/icons-material/EmojiNature';
+import TourIcon from '@mui/icons-material/Tour';
+import Footer from "./footer.jsx";
+
 
 export default function HomePage(){
     return (
         <>
             <div className="landing--page">
-                <Card>
-                    <Typography>
-                        Iam a user
-                    </Typography>
-                    <Card>
-                        <Typography>
-                            Welcome to Travell Hotel Global
-                        </Typography>
-                        <Typography>
-                            Anything you are looking for is in plenty
-                        </Typography>
-                        <Card>
-                            <Button variant="contained">Login/Register</Button>
-                        </Card>
-                    </Card>
-                </Card>
+               <div className="user--name">
+                <p>Test User</p>
+               </div>
+               <div className="more--contents">
+                <div className="content">
+                   <ConnectingAirportsIcon sx={{width: "100px", height: "50px"}}></ConnectingAirportsIcon>
+                   <p className="content--text">Travell</p>
+                </div>
+                <div className="content">
+                    <TourIcon sx={{width: "100px", height: "50px"}}></TourIcon>
+                    <p className="content--text">Tourism</p>
+                </div>
+                <div className="content">
+                    <EmojiNatureIcon sx={{width: "100px", height: "50px"}}></EmojiNatureIcon>
+                    <p className="content--text">Nature</p>
+                </div>
+               </div>
+
+               <div className="website--search--bar">
+                    <input type="search" placeholder="What are you looking for? search by name, city, price"/>
+               </div>
             </div>
-       
+            <div className="card--cover">
+            <h1>Hotels Scenerical: </h1>
+                <div className="hotels--feel">
+                        <div className="hotels--card">
+                            <img src="https://images.pexels.com/photos/1134176/pexels-photo-1134176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="an image" />
+                            <p>Coastal hotels: 20</p>
+                        </div>
+                        <div className="hotels--card">
+                            <img src="https://media.istockphoto.com/id/1400989622/photo/wooden-chalet-in-swiss-alps.jpg?s=1024x1024&w=is&k=20&c=TBYT9ifv9Welupqixad9XUbVVbK2XsVD_g1hpNpC2KE=" alt="an image" />
+                            <p>Coastal hotels: 20</p>
+                        </div>
+                        <div className="hotels--card">
+                            <img src="https://www.wandersmiles.com/wp-content/uploads/2020/05/nargakot-hotel-mystic-mountain-nepal.jpg" alt="an image" />
+                            <p>Coastal hotels: 20</p>
+                        </div>
+                </div>
+               </div>
+               <div className="card--cover">
+                <h1>Hotels in: </h1>
+                    <div className="hotels--selections">
+                        <div className="hotels--card">
+                            <img src="https://cf.bstatic.com/xdata/images/hotel/max500/391233457.jpg?k=99ab0fb976d58ff015120f1633ec6cb8446e99d4ca546356697c8ed6b5278d47&o=&hp=1" alt="" />
+                            <p>Apartment Hotels</p>
+                        </div>
+                        <div className="hotels--card">
+                            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/507295711.jpg?k=5bb9c4e5a4f329928c18c374b619034dc87ebee3ebf6201aec1b96fc7735e925&o=&hp=1" alt="" />
+                            <p>Apartment Hotels</p>
+                        </div>
+                        <div className="hotels--card">
+                            <img src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/490178393.jpg?k=c37ad91f9cb766385cff99cd4932043515857dc71e461a208039f2a5226d982b&o=&hp=1" alt="" />
+                            <p>Apartment Hotels</p>
+                        </div>
+                    </div>
+               </div>
+               <AboutPage />
+                <Footer />
         </>
     )
 }

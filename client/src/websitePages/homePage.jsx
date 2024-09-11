1ba0f8dff01077a0df3a14c57042fa752b395483
Rensytimsy <rensytimsy03@gmail.com>
2024-09-11 21:34:16 +0300

    working on the fron end

--- client/src/websitePages/homePage.jsx
+++ client/src/websitePages/homePage.jsx
@@ -3,43 +3,32 @@ import Box from "@mui/material/Box";
 import "../styleSheets/home-page.css";
 import NavBar from "../websitePages/nav.jsx";
 import AboutPage from "./about.jsx";
-
+import { Button, Card, Typography } from "@mui/material";
+import { green } from "@mui/material/colors";
+import "../styleSheets/home-page.css"
 
 export default function HomePage(){
     return (
         <>
-            <Box width="95%" height="100vh" sx={{
-                 padding: "0px ",
-                 display: "flex",
-                 flexDirection: "row"
-                 }}>
-                <Box width="100%"
-                sx={{
-                    width: "100%",
-                    position: "relative",
-                    top: "10%"
-                }}
-                >
-                    <img src="/hotel image.png" alt="" className="homepage--image"/>
-                </Box>
-                <Box sx={{
-                    width: "80%",
-                    position: "relative",
-                    top: "10%",
-                    left: "10px",   
-                    textAlign: "center"
-                }}>
-                    <p className="Welcome--header">Welcome to Travel Hotel Global🌍</p>
-                    <p className="Welcome--text">At Travel Hotel, we make discovering the perfect hotel anywhere in the world easy and enjoyable! Whether you're planning a quick getaway, a family vacation, or a business trip, our platform is designed to help you find the best accommodations that suit your needs, preferences, and budget.</p>
-                    <p className="Welcome--header">Plan Your Perfect Stay</p>
-                    <p className="Welcome--text">Simply search by location or browse our curated collections of top-rated hotels. Filter by amenities like free Wi-Fi, breakfast, or pet-friendly options. Whether you're seeking luxury, comfort, or affordability, Travel Hotel has something for everyone.
-                    Let us help you discover your next destination and make your hotel booking experience hassle-free!
-
-                    Start exploring now and find your dream hotel at Travel Hotel!
-                    </p>
-                </Box>
-            </Box>
-            <AboutPage />
+            <div className="landing--page">
+                <Card>
+                    <Typography>
+                        Iam a user
+                    </Typography>
+                    <Card>
+                        <Typography>
+                            Welcome to Travell Hotel Global
+                        </Typography>
+                        <Typography>
+                            Anything you are looking for is in plenty
+                        </Typography>
+                        <Card>
+                            <Button variant="contained">Login/Register</Button>
+                        </Card>
+                    </Card>
+                </Card>
+            </div>
+       
         </>
     )
 }
